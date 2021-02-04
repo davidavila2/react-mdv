c2868468d03e5cc32a40a0ecdebd5a3194d2a458
david <david@venturplex.com>
2021-02-03 20:37:26 -0700

    incr changes

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,33 +1,35 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function ListView(props) {
-
   if (!props.items.length) {
-    return (
-      <h1>no data</h1>
-    )
+    return <h1>no data</h1>;
   } else {
     return (
       <div className="list-container">
         <h2 className="ml-5">Items</h2>
-  
+
         <ul>
-          {props?.items?.map(item => (
-            <li key={item.id} className="item" onClick={() => props.selectItem(item)}>
+          {props?.items?.map((item) => (
+            <li
+              key={item.id}
+              className="item"
+              onClick={() => props.selectItem(item)}
+            >
               <div className="list-item">
                 <span>{item.name}</span>
                 <span>{item.description}</span>
               </div>
-  
+
               <div className="btn-actions">
                 <button onClick={() => props.delete(item.id)}>
                   <FontAwesomeIcon icon="times" />
                 </button>
 
-                <button onClick={() => props.relocate(item.id)}>
+                {/* <button onClick={() => props.relocate(item.id)}>
                   <FontAwesomeIcon icon="ellipsis-v" />
-                </button>
+                </button> */}
+                {/* Add code here to select item and display in a seperate page */}
               </div>
             </li>
           ))}
